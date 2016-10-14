3ab5098f54001110bbe28ac781ead40c3dc712f3
andip71 <andreasp@gmx.de>
2016-10-14 16:04:33 +0200

    bbuild: remove not required compiler flag

--- bbuild-anykernel.sh
+++ bbuild-anykernel.sh
@@ -14,7 +14,7 @@ BOEFFLA_VERSION="1.0-test-CM13.0-g900f"
 
 TOOLCHAIN="/opt/toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
 ARCHITECTURE=arm
-COMPILER_FLAGS_KERNEL="-Wno-array-bounds"
+COMPILER_FLAGS_KERNEL=""
 COMPILER_FLAGS_MODULE=""
 
 KERNEL_IMAGE="zImage"
